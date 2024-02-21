302593a2daa79973e15583734c8c07a6207d954c
Jack Gilman <56401347+DreamcastKnight@users.noreply.github.com>
2024-02-21 11:17:20 +0000

    [JG] OPS-11785 small amendment to generator and plugins

--- test/utils/Generators.scala
+++ test/utils/Generators.scala
@@ -17,6 +17,7 @@
 package uk.gov.hmrc.pegaproofofconceptstubs.utils
 
 import org.scalacheck.Gen
+import play.api.libs.json.Json
 import uk.gov.hmrc.pegaproofofconceptstubs.models.Payload
 
 trait Generators {
@@ -26,5 +27,5 @@ trait Generators {
   } yield str
   val nonEmptyPayload: Gen[Payload] = for {
     value <- nonEmptyStringGen
-  } yield Payload(value)
+  } yield Payload(value, "", "", Json.obj())
 }
